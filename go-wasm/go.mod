ba0692f7baa656faea6e938fc7c8d475c56f3304
root <root@Code01-Zhao>
2023-01-11 17:42:35 +0800

    go-wasm

---
+++ go-wasm/go.mod
@@ -0,0 +1,5 @@
+module github.com/VK-Link/demo-learn/go-wasm
+
+go 1.11
+
+require github.com/shurcooL/go-goon v1.0.0 // indirect
--- prometheus/monitor/main.go
+++ prometheus/monitor/main.go
@@ -1,39 +1,9 @@
+// main.go
 package main
 
-// 参考: https://cloud.tencent.com/document/product/1416/56033
-import (
-	"fmt"
-	"net/http"
-	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	klog "k8s.io/klog/v2"
-)
-
-func recordMetrics() {
-	go func() {
-		for {
-			opsProcessed.Inc()
-			time.Sleep(2 * time.Second)
-		}
-	}()
-}
-
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
-	})
-)
+import "syscall/js"
 
 func main() {
-	recordMetrics()
-	fmt.Println("server Metrics started")
-
-	klog.Info("starting gaia schedule scheduler ...")
-
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+	alert := js.Global().Get("alert")
+	alert.Invoke("Hello World!")
 }
